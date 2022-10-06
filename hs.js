2ffecf02ac87c456861de23ddd9de09af11e0423
G91688 <gultacmansimova0493@gmail.com>
2022-10-06 23:30:44 +0200

    bu ilk jsdir

---
+++ hs.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var ad     = ("Gültac")
+var soyad  = ("Mansimova-dir")
+var gün    = ("13")
+var ay     = ("04")
+var il     = ("1993")
+var reng   = ("mavi")
+var film   = ("Dark; Biohackers; The billionDollar Code")
+var aktual = ("2022")
+var is     = ("proqramist")
+
+
+document.getElementsByClassName("tje")[0].innerHTML= (`Menim adim ${ad},soyadim ise ${soyad},Men ${gün}.${ay}.${il} tarixinde dünyaya gelmisem .Menim sevdiyim ${reng} rengdir .En sevdiyim filmler  ${film}.Hal hazirda ${aktual - il}  yasim var. Men ${is} olacam.`)
